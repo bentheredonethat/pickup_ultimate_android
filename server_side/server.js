687a6366949f7b6d72d047ac922afcdd9ac0a910
bentheredonethat <ben@levinsky.org>
2016-08-17 09:08:10 -0700

    add setup code

---
+++ server_side/server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listGames', function (req, res) {
+   fs.readFile( __dirname + "/" + "games.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+app.get('/:id', function (req, res) {
+   // First read existing games.
+   fs.readFile( __dirname + "/" + "games.json", 'utf8', function (err, data) {
+       var games = JSON.parse( data );
+       var game;
+       	for(var i = 0; i < games.length; i++)
+		{
+		  if(games[i].id == req.params.id)
+		  {
+		    game = games[i];
+		  }
+		}
+
+       // var game = games[req.params.id];
+       console.log( "game: %j", game );
+       res.end( JSON.stringify(game));
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("listening at http://%s:%s", host, port)
+
+})
